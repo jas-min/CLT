267f6d8fcbe7f84e3b8d6c10795e7609514c6a37
jas-min <cabariosjasmin@gmail.com>
2020-08-09 21:55:31 -0700

    updating tests

--- tests/testthat/test-sample_mean.R
+++ tests/testthat/test-sample_mean.R
@@ -3,9 +3,9 @@ library(CLT)
 
 test_that("return a single sample mean", {
   
-  correct_result <- 5.5
+  correct_result <- any(3, 5, 8, 9, 4, 1, 6, 8, 4)
   
-  my_result <- sample_mean(c(3, 5, 8, 9, 4, 1, 6, 8, 4), 30)
+  my_result <- sample_mean(c(3, 5, 8, 9, 4, 1, 6, 8, 4), 1)
   
   expect_equal(my_result, correct_result)
 })
--- tests/testthat/test-sample_means_ns.R
+++ tests/testthat/test-sample_means_ns.R
@@ -3,9 +3,13 @@ library(CLT)
 
 test_that("returns a tibble of sample means and respective subset", {
   
-  correct_result <- as_tibble(cbind(sample.mean = c(5.40, 4.40, 5.50, 6.00, 5.60, 5.15, 6.00, 4.80, 5.95), n = c(5, 10, 20, 5, 10, 20, 5, 10, 20)))
+  correct_result <- as_tibble(cbind(sample.mean = any(c(3, 5, 8, 9, 4, 1, 6, 8, 4),
+                                                      c(3, 5, 8, 9, 4, 1, 6, 8, 4),
+                                                      c(3, 5, 8, 9, 4, 1, 6, 8, 4),
+                                                      c(3, 5, 8, 9, 4, 1, 6, 8, 4),
+                                                      c(3, 5, 8, 9, 4, 1, 6, 8, 4))), n = c(1, 1, 1, 1, 1))
   
-  my_result <- sample_means_ns(c(3, 5, 8, 9, 4, 1, 6, 8, 4), 5, c(5, 10, 20))
+  my_result <- sample_means_ns(c(3, 5, 8, 9, 4, 1, 6, 8, 4), 5, 1)
   
   expect_equal(my_result, correct_result)
 })
